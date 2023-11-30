d449c5a80dfd4721d6b257a203d88f73c11e6a33
Matt Darlington <matthewsdarlington@gmail.com>
2023-11-30 15:48:56 -0700

    Add a list of five directors to browser test the directors index and show pages with the rails server.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Director.destroy_all
+
+kubrick = Director.create!({name: "Kubrick, Stanley", years_active: 47, best_director: false})
+nolan = Director.create!({name: "Nolan, Christopher", years_active: 19, best_director: false})
+spielberg = Director.create!({name: "Spielberg, Steven", years_active: 56, best_director: true})
+scorcese = Director.create!({name: "Scorcese, Martin", years_active: 51, best_director: true})
+tarantino = Director.create!({name: "Tarantino, Quentin", years_active: 25, best_director: false})
