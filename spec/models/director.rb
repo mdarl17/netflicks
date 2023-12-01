97960c9546e655813e8b0eacfb91c8eb9f39b83e
Matt Darlington <matthewsdarlington@gmail.com>
2023-11-30 21:55:04 -0700

    Create #format_name instance method so that a director's name could be displayed as 'first middle last' name. Used primarily in director show page where sorting by director name is not necessary.

--- spec/models/director.rb
+++ spec/models/director.rb
@@ -1,7 +1,19 @@
 require "rails_helper" 
 
 RSpec.describe Director do 
-  describe 'relationships' do 
+  describe "relationships" do 
     it { should have_many :movies }
+  end
+
+  describe "instance methods" do 
+    describe "#format_name" do 
+      it "displays a director's name with first name followed by middle, if any, then last name" do 
+        kubrick = Director.create!({name: "Kubrick, Stanley", years_active: 47, best_director: false})
+        anderson = Director.create!({name: "Anderson, Paul Thomas", years_active: 25, best_director: false})
+
+        expect(kubrick.format_name).to eq("Stanley Kubrick")
+        expect(anderson.format_name).to eq("Paul Thomas Anderson")
+      end
+    end
   end
 end 
