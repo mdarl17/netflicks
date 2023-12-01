1ce54ec47e2fedb8c9cf3e97917436df7609ea63
Matt Darlington <matthewsdarlington@gmail.com>
2023-11-30 21:46:31 -0700

    Define format_name to display the name as 'first middle last'

--- app/models/director.rb
+++ app/models/director.rb
@@ -1,3 +1,9 @@
 class Director < ApplicationRecord 
   has_many :movies
+
+  def format_name 
+    name_arr = name.split(", ")
+    len = name_arr.length - 2
+    "#{name_arr.last} #{name_arr.slice(0..len).join(" ").delete(",")}"
+  end
 end
