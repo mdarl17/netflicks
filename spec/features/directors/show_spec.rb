b8cb2425c7c235b8e76b53204cffdc53b249efab
Matt Darlington <matthewsdarlington@gmail.com>
2023-12-01 14:37:21 -0700

    Add movies and move kubrick and movies to a before :each method to declutter each test and prepare for more.  Also add feature test to ensure the accurate movie count is displayed on each director's show page

--- spec/features/directors/show_spec.rb
+++ spec/features/directors/show_spec.rb
@@ -1,14 +1,27 @@
 require "rails_helper" 
 
 RSpec.describe "Director Show Page" do 
-  it "given an id, it displays a parent and that parent's attributes" do 
-    kubrick = Director.create!({name: "Kubrick, Stanley", years_active: 47, best_director: false})
+  before(:each) do 
+    @kubrick = Director.create!({name: "Kubrick, Stanley", years_active: 47, best_director: false})
+    @strangelove = Movie.create!(title: "Dr. Strangelove", released: 1964, rating: 1, sex: false, nudity: false, violence: false, director_id: @kubrick.id)
+    @shining = Movie.create!(title: "Shining, The", released: 1980, rating: 2, sex: false, nudity: true, violence: true, director_id: @kubrick.id)
+    @space_odyssey = Movie.create!(title: "2001: A Space Odyssey", released: 1968, rating: 0, sex: false, nudity: false, violence: false, director_id: @kubrick.id)
+   end
 
-    visit "/directors/#{kubrick.id}"
+  it "given an id, it displays a parent and that parent's attributes" do 
+
+    visit "/directors/#{@kubrick.id}"
 
     expect(page).to have_content("Director Details")
     expect(page).to have_content("Stanley Kubrick")
     expect(page).to have_content("Years Active: 47")
     expect(page).to have_content("Best Director Oscar: false")
+  end
+
+  it "displays the number of movies in the system that a director has made" do 
+    
+    visit "/directors/#{@kubrick.id}" 
+
+    expect(page).to have_content("Movie Count: 3")
   end
 end 
