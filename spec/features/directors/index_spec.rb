034ec0413f1c54efdb6e4a485ffa084d0eeabe19
Matt Darlington <matthewsdarlington@gmail.com>
2023-12-01 16:37:38 -0700

    Add test to make sure the 'movies' link to the movies index page is present on the page

--- spec/features/directors/index_spec.rb
+++ spec/features/directors/index_spec.rb
@@ -42,4 +42,10 @@ RSpec.describe "Directors Index Page" do
     end
   end
 
+  it "has a link to the movies index page" do 
+    visit "/directors" 
+
+    expect(page).to have_link("movies")
+  end
+
 end 
