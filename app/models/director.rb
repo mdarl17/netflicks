9db5893d23d450ebe54baeb28cf9338c7d47af82
Matt Darlington <matthewsdarlington@gmail.com>
2023-12-01 13:42:43 -0700

    Create #sort_by_created_at class method to order directors on directors index page by most recently created first.

--- app/models/director.rb
+++ app/models/director.rb
@@ -6,4 +6,8 @@ class Director < ApplicationRecord
     len = name_arr.length - 2
     "#{name_arr.last} #{name_arr.slice(0..len).join(" ").delete(",")}"
   end
+
+  def self.sort_by_created_at 
+    select("directors.*").order("created_at DESC")
+  end
 end
