d5cfad125d835ff7805dac336a330f27ebec3607
Matt Darlington <matthewsdarlington@gmail.com>
2023-11-30 21:49:26 -0700

    Define #format_title to display movies that begin with 'A' or 'The' - that currently follow the name - to pre-pend the movie title (primarily meant for movie show page where sorting by title is unnecessary)

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,14 @@
 class Movie < ApplicationRecord 
   belongs_to :director
+
+  enum rating: { "G": 0, "PG": 1, "PG-13": 2, "R": 3}
+
+  def format_title
+    if title.index(",")
+      t_arr = title.split(", ")
+      len = t_arr.length - 2
+      "#{t_arr.last} #{t_arr.slice(0..len).join(" ").delete(",")}"
+    end
+  end
+
 end
