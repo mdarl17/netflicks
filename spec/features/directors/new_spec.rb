dda2be9e9e790c3bd92c2ac12148d06863fa2168
Matt Darlington <matthewsdarlington@gmail.com>
2023-12-02 12:11:47 -0700

    Add validation testing for bad input - for both empty and wrong datatype values.  **TODO: empty strings in best_director:boolean column converting to 'true' when creating a new director. How can this be modified to raise validation error when no value is input by a user?

--- spec/features/directors/new_spec.rb
+++ spec/features/directors/new_spec.rb
@@ -20,8 +20,56 @@ RSpec.describe "New Director Page" do
 
     expect(current_path).to eq("/directors")
     expect(page).to have_content("Allen, Woody")
-
-   #TODO --> CREATE FAILING TESTS
   end
-  
+
+  it "validates all fields have a value with the correct datatype" do 
+    visit "/directors/new" 
+
+    fill_in(:name, with: " ")
+    fill_in(:years_active, with: 53)
+    fill_in(:best_director, with: true)
+
+    click_button "Create Director"
+
+    expect(current_path).to eq("/directors/new")
+    expect(Director.where("name = ?", "")).to eq([])
+
+    fill_in(:name, with: "Allen, Woody")
+    fill_in(:years_active, with: " " )
+    fill_in(:best_director, with: true)
+
+    click_button "Create Director"
+
+    expect(current_path).to eq("/directors/new")
+    expect(Director.where("name = ?", "Allen, Woody")).to eq([])
+
+    # TODO: ASK HOW TO INVALIDATE EMPTY STRING FOR ATTRIBUTE W/ BOOLEAN DATATYPE
+    # fill_in(:name, with: "Allen, Woody")
+    # fill_in(:years_active, with: 53 )
+    # fill_in(:best_director, with: " ")
+
+    # click_button "Create Director"
+
+    # expect(current_path).to eq("/directors/new")
+    # expect(Director.where("name = ?", "Allen, Woody")).to eq([])
+
+    fill_in(:name, with: 22)
+    fill_in(:years_active, with: 53)
+    fill_in(:best_director, with: true)
+
+    click_button "Create Director"
+
+    expect(current_path).to eq("/directors/new")
+    expect(Director.where("name = ?", "22")).to eq([])
+
+    fill_in(:name, with: "Allen, Woody")
+    fill_in(:years_active, with: "Fifty-Three")
+    fill_in(:best_director, with: true)
+
+    click_button "Create Director"
+
+    expect(current_path).to eq("/directors/new")
+    expect(Director.where("name = ?", "Allen, Woody")).to eq([])
+  end
+
 end 
