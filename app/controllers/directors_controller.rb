e8833e9eadce2d3b37ead466844bf23d4eb5d273
Matt Darlington <matthewsdarlington@gmail.com>
2023-12-02 00:12:19 -0700

    Define new and create actions in directors controller. Also included flash notices to test success/failure of adding a director. Validations in director model -> FIX SO BAD DATA DOESN'T BREAK PROGRAM, BUT DOES NOT INSERT INTO DB EITHER. SHOULD FAIL WITH MESSAGE. RETURN TO 'NEW' FORM PAGE WITH BAD DATA AUTO-POPULATING?

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -6,4 +6,20 @@ class DirectorsController < ApplicationController
   def show 
     @director = Director.find(params[:id])
   end
+
+  def new 
+
+  end
+
+  def create 
+    director = Director.create!({name: params[:name], years_active: params[:years_active], best_director: params[:best_director]})
+
+    unless director
+      flash[:notice] = "Sorry, something went wrong. Please try again."
+      redirect_to "/directors/new"
+    end
+    
+    flash[:notice] = "New director created."
+    redirect_to "/directors"
+  end
 end
