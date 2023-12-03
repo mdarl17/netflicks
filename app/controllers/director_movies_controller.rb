5d4252a0e3950d5dd2a3e912a60cd2a287d12dd8
Matt Darlington <matthewsdarlington@gmail.com>
2023-12-03 12:31:59 -0700

    Define new and create actions in director movies controller, allowing users to enter a new movie for a specific director.

--- app/controllers/director_movies_controller.rb
+++ app/controllers/director_movies_controller.rb
@@ -3,4 +3,29 @@ class DirectorMoviesController < ApplicationController
     @director = Director.find(params[:id])
     @movies = @director.movies
   end
+
+  def new 
+    @director = Director.find(params[:id])
+  end
+
+  def create 
+    director = Director.find(params[:id])
+
+    movie = director.movies.new({
+      title: params[:title],
+      released: params[:released],
+      rating: params[:rating].to_i,
+      sex: params[:sex],
+      nudity: params[:nudity],
+      violence: params[:violence]
+    })
+
+    if movie.save
+      flash[:notice] = "A new movie has been successfully added."
+    else
+      flash[:notice] = "Sorry, an error has ocurred. The movie was not added to the system. Please try again."
+    end
+    redirect_to "/directors/#{director.id}/movies"
+  end
+
 end
