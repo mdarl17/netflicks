0a4c6c0a9f82050742936437f9473c0acbffe9f6
Matt Darlington <matthewsdarlington@gmail.com>
2023-12-03 18:06:13 -0700

    Modify validations in attempt to raise error (quietly, don't break execution) when any attribute is empty or of the wrong datatype. TODO This will need further research and assistance

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,11 +1,11 @@
 class Movie < ApplicationRecord 
   belongs_to :director
   
-  validates :title, format: { with: /[a-zA-Z]/ }
-  validates :released, numericality: { only_integer: true }
-  validates :rating, numericality: { only_integer: true }
-  validates :rating, presence: true
-  validates :sex, :nudity, :violence, inclusion: [true, false]
+  validates :title, format: { with: /[a-zA-Z]/ }, presence: true
+  validates :released, :rating, numericality: { only_integer: true }, presence: true
+  validates :sex, inclusion: [true, false]
+  validates :nudity, inclusion: [true, false]
+  validates :violence, inclusion: [true, false]
 
   enum rating: { "G": 0, "PG": 1, "PG-13": 2, "R": 3}
 
