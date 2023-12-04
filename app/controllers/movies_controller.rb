39e21685e152d35958062aaea9b015153ef159a7
Matt Darlington <matthewsdarlington@gmail.com>
2023-12-03 18:04:10 -0700

    Define edit and update methods so users can edit movie attributes and save to database

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,4 +6,29 @@ class MoviesController < ApplicationController
   def show 
     @movie = Movie.find(params[:id])
   end
+
+  def edit 
+    @movie = Movie.find(params[:id])
+  end
+
+  def update 
+    movie = Movie.find(params[:id])
+    director = movie.director
+
+    if movie.update({
+      title: params[:title],
+      released: params[:released],
+      rating: params[:rating].to_i,
+      sex: params[:sex],
+      nudity: params[:nudity],
+      violence: params[:violence],
+      director_id: director.id
+    })
+      flash[:notice] = "The movie has been successfully updated."
+      redirect_to "/movies/#{movie.id}"
+    else
+      flash[:notice] = "Sorry, we were not able to update any movie data. Please try again." 
+      redirect_to "/movies/#{movie.id}/edit"
+    end
+  end
 end
