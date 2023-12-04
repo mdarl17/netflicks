03dd235a7f54bd713ffa7f6f0b5c3d75a1ec24b6
Matt Darlington <matthewsdarlington@gmail.com>
2023-12-03 19:38:42 -0700

    Comment out feature tests for original movie index that displayed all movies in the system. Per US-15, create categories based on boolean columns - adult content and violence. Some movies are displayed twice, and any movie that doesn't have any adult content or violence will not be displayed.

--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -10,52 +10,60 @@ RSpec.describe "Movies Index Page" do
 
     @strangelove = Movie.create!(title: "Dr. Strangelove", released: 1964, rating: 1, sex: false, nudity: false, violence: false, director_id: @kubrick.id)
     @shining = Movie.create!(title: "Shining, The", released: 1980, rating: 2, sex: false, nudity: true, violence: true, director_id: @kubrick.id)
-    @jaws = Movie.create!(title: "Jaws", released: 1975, rating: 1, sex: false, nudity: false, violence: true, director_id: @spielberg.id)
+    @jaws = Movie.create!(title: "Jaws", released: 1975, rating: 1, sex: false, nudity: false, violence: false, director_id: @spielberg.id)
     @dark_knight = Movie.create!(title: "Dark Knight, The", released: 2008, rating: 2, sex: false, nudity: false, violence: true, director_id: @nolan.id)
     @goodfellas = Movie.create!(title: "Goodfellas", released: 1990, rating: 3, sex: true, nudity: true, violence: true, director_id: @scorcese.id)
-    @pulp_fiction = Movie.create!(title: "Pulp Fiction", released: 1994, rating: 3, sex: false, nudity: false, violence: true, director_id: @tarantino.id)
+    @pulp_fiction = Movie.create!(title: "Pulp Fiction", released: 1994, rating: 3, sex: true, nudity: true, violence: true, director_id: @tarantino.id)
   end
 
-  it "displays a list of all the movies - and its atrributes - in the system" do 
-    visit "/movies" 
+  #   it "displays a list of all the movies - and its atrributes - in the system" do 
+  #   visit "/movies" 
 
-    expect(page).to have_content("Movie List")
+    # Header before US-15: Display True Boolean values
 
-    expect(page).to have_content("Dr. Strangelove")
-    expect(page).to have_content("Released: 1964")
-    expect(page).to have_content("Rating: PG")
-    expect(page).to have_content("Sex: false")
-    expect(page).to have_content("Nudity: false")
-    expect(page).to have_content("Violence: false")
+    # expect(page).to have_content("Movie List")
 
-    expect(page).to have_content("Jaws")
-    expect(page).to have_content("Released: 1975")
-    expect(page).to have_content("Rating: PG")
-    expect(page).to have_content("Sex: false")
-    expect(page).to have_content("Nudity: false")
-    expect(page).to have_content("Violence: true")
+  #   Initial Movie Index that displayed all movies in system
+  #   US-15 modified view to only display movies with 'true' boolean values
+  #   My project had three boolean columns, so most movies still shown in one of the two sections
+  #   I grouped 'sex' and 'nudity' into one categore => Adult Content
+  #   Only movies with NO true boolean values aren't displayed
 
-    expect(page).to have_content("Dark Knight, The")
-    expect(page).to have_content("Released: 2008")
-    expect(page).to have_content("Rating: PG-13")
-    expect(page).to have_content("Sex: false")
-    expect(page).to have_content("Nudity: false")
-    expect(page).to have_content("Violence: true")
+  #   expect(page).to have_content("Dr. Strangelove")
+  #   expect(page).to have_content("Released: 1964")
+  #   expect(page).to have_content("Rating: PG")
+  #   expect(page).to have_content("Sex: false")
+  #   expect(page).to have_content("Nudity: false")
+  #   expect(page).to have_content("Violence: false")
 
-    expect(page).to have_content("Goodfellas")
-    expect(page).to have_content("Released: 1990")
-    expect(page).to have_content("Rating: R")
-    expect(page).to have_content("Sex: true")
-    expect(page).to have_content("Nudity: true")
-    expect(page).to have_content("Violence: true")
+  #   expect(page).to have_content("Jaws")
+  #   expect(page).to have_content("Released: 1975")
+  #   expect(page).to have_content("Rating: PG")
+  #   expect(page).to have_content("Sex: false")
+  #   expect(page).to have_content("Nudity: false")
+  #   expect(page).to have_content("Violence: false")
 
-    expect(page).to have_content("Pulp Fiction")
-    expect(page).to have_content("Released: 1994")
-    expect(page).to have_content("Rating: R")
-    expect(page).to have_content("Sex: false")
-    expect(page).to have_content("Nudity: false")
-    expect(page).to have_content("Violence: true")
-  end
+  #   expect(page).to have_content("Dark Knight, The")
+  #   expect(page).to have_content("Released: 2008")
+  #   expect(page).to have_content("Rating: PG-13")
+  #   expect(page).to have_content("Sex: false")
+  #   expect(page).to have_content("Nudity: false")
+  #   expect(page).to have_content("Violence: true")
+
+  #   expect(page).to have_content("Goodfellas")
+  #   expect(page).to have_content("Released: 1990")
+  #   expect(page).to have_content("Rating: R")
+  #   expect(page).to have_content("Sex: true")
+  #   expect(page).to have_content("Nudity: true")
+  #   expect(page).to have_content("Violence: true")
+
+  #   expect(page).to have_content("Pulp Fiction")
+  #   expect(page).to have_content("Released: 1994")
+  #   expect(page).to have_content("Rating: R")
+  #   expect(page).to have_content("Sex: true")
+  #   expect(page).to have_content("Nudity: false")
+  #   expect(page).to have_content("Violence: true")
+  # end
 
   it "has a link to the directors index page" do 
     visit "/movies"
@@ -65,6 +73,33 @@ RSpec.describe "Movies Index Page" do
     click_link "directors" 
 
     expect(current_path).to eq("/directors")
+  end
+
+  describe "true boolean fields" do 
+    it "has a section that lists movies with adult content (i.e. sex, nudity)" do 
+      visit "/movies"
+
+      within "#index-adult-content" do 
+        expect(page).to have_content("Movies that Contain Adult Content")
+        expect(page).to have_content("Shining, The")
+        expect(page).to have_content("Goodfellas")
+        expect(page).to have_content("Pulp Fiction")
+      end
+    end
+
+    it "has a section that lists movies that have violent content" do 
+      visit "/movies"
+      
+      within "#index-violence" do
+        expect(page).to have_content("Movies that Contain Violence")
+        expect(page).to have_content("Shining, The")
+        expect(page).to have_content("Dark Knight, The")
+        expect(page).to have_content("Goodfellas")
+        expect(page).to have_content("Pulp Fiction")
+        
+        save_and_open_page
+      end
+    end
   end
 
 end 
