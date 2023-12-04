b0476a223fecc419eab2ca638c406fc237270233
Matt Darlington <matthewsdarlington@gmail.com>
2023-12-04 16:41:51 -0700

    Add feature testing to ensure 'edit' links are present next to each director's name and to confirm they redirect to director edit page and successfully update a director's information.

--- spec/features/directors/index_spec.rb
+++ spec/features/directors/index_spec.rb
@@ -3,11 +3,11 @@ require 'date'
 
 RSpec.describe "Directors Index Page" do 
   before(:each) do 
-    kubrick = Director.create!({name: "Kubrick, Stanley", years_active: 47, best_director: false, created_at: Date.today - 3})
-    nolan = Director.create!({name: "Nolan, Christopher", years_active: 19, best_director: false, created_at: Date.today - 1})
-    spielberg = Director.create!({name: "Spielberg, Steven", years_active: 56, best_director: true, created_at: Date.today})
-    scorcese = Director.create!({name: "Scorcese, Martin", years_active: 51, best_director: true, created_at: Date.today - 4})
-    tarantino = Director.create!({name: "Tarantino, Quentin", years_active: 25, best_director: false, created_at: Date.today - 2})
+    @kubrick = Director.create!({name: "Kubrick, Stanley", years_active: 47, best_director: false, created_at: Date.today - 3})
+    @nolan = Director.create!({name: "Nolan, Christopher", years_active: 19, best_director: false, created_at: Date.today - 1})
+    @spielberg = Director.create!({name: "Spielberg, Steven", years_active: 56, best_director: true, created_at: Date.today})
+    @scorcese = Director.create!({name: "Scorcese, Martin", years_active: 51, best_director: true, created_at: Date.today - 4})
+    @tarantino = Director.create!({name: "Tarantino, Quentin", years_active: 25, best_director: false, created_at: Date.today - 2})
   end
 
   it "displays the name of each director in the system" do 
@@ -66,6 +66,43 @@ RSpec.describe "Directors Index Page" do
       expect(current_path).to eq("/directors/new")
       expect(page).to have_content("New Director")
     end 
+  end
+
+  describe "updating a director's information" do 
+    it "next to every director is a link to edit the director's information" do 
+      visit "/directors/"
+
+      directors = Director.all
+
+      directors.each do |director| 
+        within "#index-#{director.id}" do 
+          expect(page).to have_link("edit")
+        end
+      end
+    end
+
+    it "when the 'edit' link is clicked, users are redirected to the director edit page where they can update a director's info." do 
+      visit "/directors"
+
+      within "#index-#{@nolan.id}" do 
+        click_link "edit"
+        expect(current_path).to eq("/directors/#{@nolan.id}/edit")
+      end
+      expect(page).to have_content("Update #{@nolan.format_name}'s Bio")
+
+      fill_in(:name, with: "Nolan, Christopher Jonathan James")
+      fill_in(:years_active, with: 25)
+      fill_in(:best_director, with: true)
+
+      click_button "Update"
+
+      expect(current_path).to eq("/directors/#{@nolan.id}")
+
+      expect(page).to have_content("Christopher Jonathan James Nolan")
+      expect(page).to have_content("Years Active: 25")
+      expect(page).to have_content("Best Director Oscar: true")
+      expect(page).to have_content("The director's bio information has been successfully updated.")
+    end
   end
   
 end 
