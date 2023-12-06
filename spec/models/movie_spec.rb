5ffb09ccc366f0c9044ae7d9c4838b56b477b325
Matt Darlington <matthewsdarlington@gmail.com>
2023-12-06 15:41:38 -0700

    Add many new test data validations for records in movie model. WIP: unexpected/unwanted behavior (almost always evaluating to 'truthy') of boolean fields re: empty strings and incorrect datatypes.

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -20,6 +20,39 @@ RSpec.describe Movie do
     @pulp_fiction = Movie.create!(title: "Pulp Fiction", released: 1994, rating: 3, sex: true, nudity: true, violence: true, director_id: @tarantino.id)
    end
 
+  describe "validations" do 
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :released }
+    it { should validate_presence_of :rating }
+
+    # TODO why are valid boolean values returning 'false' even though they are present? (e.g. setup data failing)
+
+    # it { should validate_presence_of :sex }
+    # it { should validate_presence_of :nudity }
+    # it { should validate_presence_of :violence }
+
+    it { should validate_numericality_of :released }
+
+    it {should allow_value(/[a-zA-Z]/).for :title }
+    it {should allow_value('G').for :rating}
+    it {should allow_value('PG').for :rating}
+    it {should allow_value('PG-13').for :rating}
+    it {should allow_value('R').for :rating}
+    it {should allow_value(true).for :sex }
+    it {should allow_value(false).for :sex }
+    it {should_not allow_value(nil).for :sex }
+
+    it {should allow_value(true).for :nudity }
+    it {should allow_value(false).for :nudity }
+    it {should_not allow_value(nil).for :nudity }
+
+    it {should allow_value(true).for :violence }
+    it {should allow_value(false).for :violence }
+    it {should_not allow_value(nil).for :violence }
+
+    it { should validate_length_of(:released), minimum: 0, maximum: Time.now.year }
+  end
+
   describe "relationships" do 
     it { should belong_to :director }
   end
